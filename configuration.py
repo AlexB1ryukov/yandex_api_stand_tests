cce7278b76c98eeaa45af12f565b733bcd2ddf3b
Александр Бирюков <alexanderbiryukov@MacBook-Air-Aleksandr.local>
2024-10-13 19:25:58 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://b035b21a-9d3b-4542-9a03-dd841c953f71.serverhub.praktikum-services.ru"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
